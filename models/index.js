fc29b6bce0a672f28aa56ace63de9d65cbea53b4
nathanallen <thenathanator@gmail.com>
2015-12-09 13:57:23 -0800

    solution 2.1 Actór schema

--- models/index.js
+++ models/index.js
@@ -3,3 +3,4 @@ mongoose.connect('mongodb://localhost/my-tv-db-p2');
 
 module.exports.Character = require("./character.js")
 module.exports.Show = require("./show.js")
+module.exports.Actor = require("./actor.js")
